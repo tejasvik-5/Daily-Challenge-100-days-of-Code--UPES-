32f0aff477392577b30b1b33d0ffd842c18416e5
tejasvik-5 <tkandpal007@gmail.com>
2025-09-18 16:43:32 +0530

    Added day1 challenge code

--- Day1.c/ques1.c
+++ Day1.c/ques1.c
@@ -2,9 +2,12 @@
 int main(){
     // Write a program to input two numbers and display their sum.
     int a,b;
-    printf("Enter two numbers");
-    scanf("%d %d", &a, &b);
+    printf("Enter first number");
+    scanf("%d", &a);
+    printf("Enter second number");
+    scanf("%d",&b);
     int sum = a + b;
     printf("sum of two numbers: %d", sum);
-    return 0;
-} 
+   return 0;
+}
+    
